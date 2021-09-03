880856ce8f2e8c7133cf0e21306b65fa3a0a0f71
Colby Fayock <fayock@gmail.com>
2021-09-03 15:26:16 -0400

    updating to usebeforeall and aftearll

--- src/test/java/com/applitools/quickstarts/FluentAPI.java
+++ src/test/java/com/applitools/quickstarts/FluentAPI.java
@@ -9,37 +9,48 @@ import com.applitools.eyes.selenium.fluent.Target;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.junit.jupiter.api.Test;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterAll;
 
 public class FluentAPI {
 
-	@Test
-  public void test() {
-		WebDriver driver = new ChromeDriver(new ChromeOptions());
-		EyesRunner runner = new ClassicRunner();
-		Eyes eyes = new Eyes(runner);
-		Configuration config = new Configuration();
-	
+	static WebDriver driver;
+	static EyesRunner runner;
+	static Eyes eyes;
+	static Configuration config;
+
+	@BeforeAll
+	public static void init(){
+		driver = new ChromeDriver(new ChromeOptions());
+		runner = new ClassicRunner();
+		eyes = new Eyes(runner);
+		config = eyes.getConfiguration();
+
 		config.setBatch(new BatchInfo("Demo Batch - Selenium for Java - Fluent API"));
 		eyes.setConfiguration(config);
-		
-		try {
-			eyes.open(driver, "Demo App - Selenium for Java - Fluent API", "Smoke Test - Selenium for Java - Fluent API", new RectangleSize(800, 600));
-
-			driver.get("https://demo.applitools.com");
-
-			eyes.checkWindow("Login Window");
-
-			driver.findElement(By.id("log-in")).click();
-
-			eyes.check(Target.window().fully().withName("App Window"));
-			eyes.check("Transactions Table", Target.region(By.className("table")));
-
-			eyes.closeAsync();
-		} finally {
-			driver.quit();
-		}
 	}
 
+	@Test
+  public void test() {
+		eyes.open(driver, "Demo App - Selenium for Java - Fluent API", "Smoke Test - Selenium for Java - Fluent API", new RectangleSize(800, 600));
+
+		driver.get("https://demo.applitools.com");
+
+		eyes.check("Login Window", Target.window().fully());
+
+		driver.findElement(By.id("log-in")).click();
+
+		eyes.check(Target.window().fully().withName("App Window"));
+		eyes.check("Transactions Table", Target.region(By.className("table")));
+
+		eyes.closeAsync();
+	}
+
+	@AfterAll
+	public static void cleanUp(){
+		eyes.abortAsync();
+		driver.quit();
+	}
 }
--- src/test/java/com/applitools/quickstarts/VisualLocators.java
+++ src/test/java/com/applitools/quickstarts/VisualLocators.java
@@ -11,54 +11,63 @@ import com.applitools.eyes.Region;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.junit.jupiter.api.Test;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterAll;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class VisualLocators {
 
-	@Test
-  public void test() {
-		WebDriver driver = new ChromeDriver(new ChromeOptions());
-		EyesRunner runner = new ClassicRunner();
-		Eyes eyes = new Eyes(runner);
-		Configuration config = new Configuration();
-	
+	static WebDriver driver;
+	static EyesRunner runner;
+	static Eyes eyes;
+	static Configuration config;
+
+	@BeforeAll
+	public static void init(){
+		driver = new ChromeDriver(new ChromeOptions());
+		runner = new ClassicRunner();
+		eyes = new Eyes(runner);
+		config = eyes.getConfiguration();
+
 		config.setBatch(new BatchInfo("Demo Batch - Selenium for Java - Visual Locators"));
 		eyes.setConfiguration(config);
-		
-		try {
-			driver.get("https://demo.applitools.com/login-interactive.html");
-			
-			eyes.open(driver, "Applitools.com - Selenium for Java - Visual Locators", "Smoke Test - Selenium for Java - Visual Locators", new RectangleSize(800, 600));
-
-			Map<String, List<Region>> locators = eyes.locate(VisualLocator.name("linkedin-logo"));
-
-			List<Region> locatorRegions = locators.get("linkedin-logo");
-			
-			Region regionLinkedInLogo = locatorRegions.get(0);
-
-			int clickLocationX = regionLinkedInLogo.getLeft() + regionLinkedInLogo.getWidth() / 2;
-			int clickLocationY = regionLinkedInLogo.getTop() + regionLinkedInLogo.getHeight() / 2;
-
-			eyes.checkWindow("Login Window");
-
-			new Actions(driver)
-				.moveByOffset(clickLocationX, clickLocationY)
-				.click()
-				.perform();
-
-			eyes.checkWindow("LinkedIn");
-
-			eyes.closeAsync();
-		} finally {
-			eyes.abortAsync();
-			driver.quit();
-		}
 	}
 
+	@Test
+  public void test() {
+		driver.get("https://demo.applitools.com/login-interactive.html");
+		
+		eyes.open(driver, "Applitools.com - Selenium for Java - Visual Locators", "Smoke Test - Selenium for Java - Visual Locators", new RectangleSize(800, 600));
+
+		Map<String, List<Region>> locators = eyes.locate(VisualLocator.name("linkedin-logo"));
+
+		List<Region> locatorRegions = locators.get("linkedin-logo");
+		
+		Region regionLinkedInLogo = locatorRegions.get(0);
+
+		int clickLocationX = regionLinkedInLogo.getLeft() + regionLinkedInLogo.getWidth() / 2;
+		int clickLocationY = regionLinkedInLogo.getTop() + regionLinkedInLogo.getHeight() / 2;
+
+		eyes.check("Login Window", Target.window().fully());
+
+		new Actions(driver)
+			.moveByOffset(clickLocationX, clickLocationY)
+			.click()
+			.perform();
+
+		eyes.check("LinkedIn", Target.window().fully());
+
+		eyes.closeAsync();
+	}
+
+	@AfterAll
+	public static void cleanUp(){
+		eyes.abortAsync();
+		driver.quit();
+	}
 }
